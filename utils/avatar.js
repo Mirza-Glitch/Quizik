38cb8b610199da98bf58cc592db89c4b36d97266
Mirza-Glitch <89267349+Mirza-Glitch@users.noreply.github.com>
2023-06-04 21:34:17 +0530

    Update avatar.js
    Changed avatar image type from fun-emoji to thumbs

--- utils/avatar.js
+++ utils/avatar.js
@@ -2,8 +2,8 @@ import { createAvatar } from "@dicebear/core";
 import { funEmoji } from '@dicebear/collection';
 
 function getAvatar(name) {
-  // const avatar = createAvatar(thumbs, { seed: name });
-  const avatar = createAvatar(funEmoji, { seed: name });
+  const avatar = createAvatar(thumbs, { seed: name });
+  // const avatar = createAvatar(funEmoji, { seed: name });
   const imgString = avatar.toDataUri();
   return imgString;
 }
